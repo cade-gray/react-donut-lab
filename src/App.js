536f82adfc887f1a14eb05b42ba435383b3d5968
Cade Gray <cgray35@radford.edu>
2020-04-15 15:17:46 -0400

    Upload to github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [donuts, setDonuts] = useState(
+    [
+      {
+        name: "Chocolate Donut",
+        batter: "Chocolate",
+        toppings: ["Chocolate Frosting", "Sprinkles"]
+      },
+      {
+        name: "Regular Donut",
+        batter: "Vanilla",
+        toppings: ["Regular Glaze"]
+      },
+      {
+        name: "Mimosa Donut",
+        batter: "Champagne",
+        toppings: ["Orange Glaze", "Strawberries"]
+      }
+    ]
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Cade's Donut Shop</h1>
+      <div className="donuts">
+        {donuts.map((donut) => {
+          return(
+          <Donut name={donut.name} batter={donut.batter} >
+            {donut.toppings.map(topping => {return <h4>{topping}</h4>})}
+          </Donut>  
+          )
+        })}
+      </div>
     </div>
   );
+}
+
+function Donut(props) {
+  return(
+    <div className="donut" >
+      <h2>{props.name}</h2>
+      <h3>{props.batter} Batter</h3>
+      <h3><u>Topping(s)</u></h3>
+        {props.children}
+    </div>
+  )
 }
 
 export default App;
