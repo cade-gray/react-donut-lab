f841ce8b36eda91a195e467861740df3ce74497f
Cade Gray <cgray35@radford.edu>
2020-04-15 15:21:20 -0400

    Updates

---
+++ precache-manifest.da5adeeb39465bab717d4d3fdcc5971c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82634d0914e447be72b5a9e7e39f53ef",
+    "url": "/react-donut-lab/index.html"
+  },
+  {
+    "revision": "aa21b8a7fe49804ab222",
+    "url": "/react-donut-lab/static/css/main.b680597a.chunk.css"
+  },
+  {
+    "revision": "32c3e7d17ac29c5edf77",
+    "url": "/react-donut-lab/static/js/2.b8082492.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-donut-lab/static/js/2.b8082492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa21b8a7fe49804ab222",
+    "url": "/react-donut-lab/static/js/main.3934bed4.chunk.js"
+  },
+  {
+    "revision": "9b24ed9486d013dc1b2e",
+    "url": "/react-donut-lab/static/js/runtime-main.6fea1523.js"
+  }
+]);
